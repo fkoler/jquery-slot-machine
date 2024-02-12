12951f03083b2eada5645a38073ef7600f90c8b0
ferenc_koler <ferenckoler@outlook.com>
2024-02-12 23:48:41 +0100

    Update

--- script.js
+++ script.js
@@ -1,94 +1,99 @@
-$(document).ready(function() {
-    var firstPix = $("#first img");
-    var secondPix = $("#second img");
-    var thirdPix = $("#third img");
-    var credit = parseInt(prompt("Insert Credit", 500));
-    $("#num").html(credit);
+$(document).ready(() => {
+    let credit;
 
-    var pix = ["img/apple.png", "img/bar.png", "img/bell.png", "img/cherry.png", "img/grape.png", "img/heart.png", "img/lemon.png", "img/seven.png", "img/waterm.png"];
+    do {
+        credit = +prompt('Insert Credit', 500);
+    } while (isNaN(credit) || credit <= 0 || credit % 100 !== 0);
 
-    var rand1;
-    var rand2;
-    var rand3;
+    $('#num').html(credit);
 
-    function generateRandom() {
-        rand1 = Math.floor(Math.random() * pix.length);
-        rand2 = Math.floor(Math.random() * pix.length);
-        rand3 = Math.floor(Math.random() * pix.length);
-        console.log(rand1 + " " + rand2 + " " + rand3);
-        firstPix.attr("src", pix[rand1]);
-        secondPix.attr("src", pix[rand2]);
-        thirdPix.attr("src", pix[rand3]);
+    const firstPic = $('#first img');
+    const secondPic = $('#second img');
+    const thirdPic = $('#third img');
+
+    const pix = [
+        'img/apple.png',
+        'img/bar.png',
+        'img/bell.png',
+        'img/cherry.png',
+        'img/grape.png',
+        'img/heart.png',
+        'img/lemon.png',
+        'img/seven.png',
+        'img/waterm.png'
+    ];
+
+    const sound1 = new Audio('sounds/win.mp3');
+    const sound2 = new Audio('sounds/loose.mp3');
+    const sound3 = new Audio('sounds/jackpot.mp3');
+    const sound4 = new Audio('sounds/push.mp3');
+
+    const generateRandom = () => Math.floor(Math.random() * pix.length);
+
+    const updateCredit = (amount) => {
+        credit += amount;
+        $('#num').html(credit);
     };
 
-    $("#spin").click(function() {       
+    const displayResult = (message, color) => {
+        $('#rez').html(message).css('color', color).css('font-weight', 'bold');
+    };
 
-        if(credit > 0){
-            var interval = setInterval(generateRandom, 100);
-            $("#rez").html("");
+    const handleSpinResult = () => {
+        if (rand1 === rand2 && rand2 === rand3) {
+            displayResult('JACKPOT!! YOU WON 1500 &euro;!!', 'green');
+            updateCredit(1500);
+            $('#spin').attr('disabled', false);
+            sound3.play();
+        } else if (
+            (rand1 === rand2 && rand2 !== rand3) ||
+            (rand2 === rand3 && rand1 !== rand3) ||
+            (rand1 === rand3 && rand1 !== rand2)
+        ) {
+            displayResult('YOU WON 200 &euro;!', 'blue');
+            updateCredit(200);
+            $('#spin').attr('disabled', false);
+            sound1.play();
+        } else {
+            displayResult('You Lost 100 &euro;', 'black');
+            updateCredit(-100);
+            $('#spin').attr('disabled', false);
+            sound2.play();
+        };
+    };
 
-            var sound1 = {
-                push: new Howl({
-                    src: ["sounds/win.mp3"]
-                })
-            };
-            
-            var sound2 = {
-                push: new Howl({
-                    src: ["sounds/loose.mp3"]
-                })
-            };
-            
-            var sound3 = {
-                push: new Howl({
-                    src: ["sounds/jackpot.mp3"]
-                })
-            };
-            
-            var sound4 = {
-                push: new Howl({
-                    src: ["sounds/push.mp3"]
-                })
-            };
+    $('#spin').click(() => {
+        $('#spin').attr('disabled', true);
 
-            sound4.push.play();
+        if (credit > 0) {
+            sound4.play();
+            const interval = setInterval(() => {
+                rand1 = generateRandom();
+                rand2 = generateRandom();
+                rand3 = generateRandom();
 
-            setTimeout(function() {
-                if(rand1 == rand2 && rand2 == rand3) {
-                    console.log("JACKPOT!! YOU WON 1500!!");
-                    $("#rez").html("JACKPOT!! YOU WON 1500 &euro;!!");
-                    credit += 1500;
-                    sound3.push.play();
-                    $("#num").html(credit);
-                } else if ((rand1 == rand2 && rand2 != rand3) || 
-                (rand2 == rand3 && rand1 != rand3) ||
-                (rand1 == rand3 && rand1 != rand2)) {
-                    console.log("YOU WON 200!");
-                    $("#rez").html("YOU WON 200 &euro;!");
-                    credit += 200;
-                    sound1.push.play();
-                    $("#num").html(credit);
-                    $("#rez").css("color", "blue").css("font-weight", "bold");
-                } else {
-                    console.log("You Lost 100");
-                    $("#rez").html("You Lost 100 &euro;");
-                    credit -= 100;
-                    sound2.push.play();
-                    $("#num").html(credit);
-                    $("#rez").css("color", "black").css("font-weight", "bold");
-                };
+                firstPic.attr('src', pix[rand1]);
+                secondPic.attr('src', pix[rand2]);
+                thirdPic.attr('src', pix[rand3]);
+            }, 100);
+
+            $('#rez').html('');
+
+            setTimeout(() => {
+                handleSpinResult();
                 clearInterval(interval);
             }, 3500);
-            
-        } else {
-            $("#rez").html("GAME OVER !");
-            $("#spin").attr("disabled", "true");
-            $("#rez").css("color", "red").css("font-size", "50px").css("font-weight", "bold");
         };
 
-        $("#newGame").click(function() {
-            location.reload();
-        });
+        if (credit === 0) {
+            $('#spin').attr('display', 'none');
+            $('#newGame').attr('display', 'inline-block');
+            $('#rez').html('GAME OVER!');
+            $('#rez').css('color', 'red').css('font-size', '50px').css('font-weight', 'bold');
+        };
+    });
 
+    $('#newGame').click(() => {
+        location.reload();
     });
 });
